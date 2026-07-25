edbb9e013e182460c875137eaf313fa878287bb0
Nelson Elielton Bolkota <bolkotanelson@gmail.com>
2026-07-25 10:38:04 -0300

    2

--- atividades/2.php
+++ atividades/2.php
@@ -1,4 +1,15 @@
 <?php
 // Atividade 2
+$dinheiro = 1621;
 
-?>
+if ($dinheiro <= 1621) {
+    echo "Pobre\n";
+} elseif ($dinheiro < 10000) {
+    echo "Classe Media\n";
+} elseif ($dinheiro < 100000) {
+    echo "Riquinho\n";
+} elseif ($dinheiro < 1000000) {
+    echo "Ricão\n";
+} else {
+    echo "Elon Musk\n";
+}
