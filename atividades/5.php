feba312c84c63737ea44e1504e20c1de59a08d94
Nelson Elielton Bolkota <bolkotanelson@gmail.com>
2026-07-27 19:35:12 -0300

    sinco

--- atividades/5.php
+++ atividades/5.php
@@ -1,4 +1,17 @@
 <?php
 // Atividade 5
-
+for($i = 0; $i < 11 ; $i++){
+    $resultado = 4 * $i;
+    echo "4 x ".$i." = ".$resultado."\n";
+}
+echo"=========================\n";
+for($i = 0; $i < 11 ; $i++){
+    $resultado = 7 * $i;
+    echo "7 x ".$i." = ".$resultado."\n";
+}
+echo"=========================\n";
+for($i = 0; $i < 11 ; $i++){
+    $resultado = 12879.5 * $i;
+    echo "12.879,5 x ".$i." = ".$resultado."\n";
+}
 ?>
