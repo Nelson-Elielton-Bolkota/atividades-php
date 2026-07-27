391c6b0f633148ae39940c90b8a21af5cbd6baa5
Nelson Elielton Bolkota <bolkotanelson@gmail.com>
2026-07-27 19:15:13 -0300

    3

--- atividades/3.php
+++ atividades/3.php
@@ -1,4 +1,33 @@
 <?php
 // Atividade 3
+$numero1 = intval(readline("digite o 1° numero: \n"));
+$numero2 = intval(readline("digite o 2° numero: \n"));
+$operacao = readline("digite 1 para soma:\n
+digite 2 para subtração:\n
+digite 3 para multiplicação:\n
+digite 4 para divisão:\n");
 
+switch ($operacao) {
+    case '1':
+        $resultado = $numero1 + $numero2;
+        break;
+
+    case '2':
+        $resultado = $numero1 - $numero2;
+        break;
+
+    case '3':
+        $resultado = $numero1 * $numero2;
+        break;
+
+    case '4':
+        $resultado = $numero1 / $numero2;
+        break;
+    
+    default:
+        echo "opção invalida";
+        break;
+    
+}
+echo "Resultado: " . $resultado . "\n";
 ?>
