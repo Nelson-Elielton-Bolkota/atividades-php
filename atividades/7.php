ffe95d84042bd7cb7502bf0c30f96652584cf69a
Nelson Elielton Bolkota <bolkotanelson@gmail.com>
2026-07-27 22:32:03 -0300

    seven

--- atividades/7.php
+++ atividades/7.php
@@ -1,4 +1,13 @@
 <?php
-// Atividade 7
 
+function soma($a, $b){
+    $soma = $a + $b;
+    $sub = $a - $b;
+    echo "".$soma."\n";
+    echo $sub."\n";
+    echo "frase incrivel\n";
+    return;
+}
+
+soma(10,5);
 ?>
