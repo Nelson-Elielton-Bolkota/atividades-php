0f05264980486e16bd9cbc70f5ce9e34e916d7f7
Nelson Elielton Bolkota <bolkotanelson@gmail.com>
2026-07-27 22:49:29 -0300

    jitica

--- atividades/9.php
+++ atividades/9.php
@@ -1,4 +1,10 @@
 <?php
 // Atividade 9
+$papagaios = ["tiriva", "tucano", "bentivi", "joaninha", "jitica"];
 
+$papagaios[] = "aveztruz";
+
+foreach ($papagaios as $papagaio) {
+    echo $papagaio."\n";
+}
 ?>
