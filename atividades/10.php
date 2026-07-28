0f43a7c6a2c8008508f59815d9b9d020924583c3
Nelson Elielton Bolkota <bolkotanelson@gmail.com>
2026-07-27 22:57:33 -0300

    deiz

--- atividades/10.php
+++ atividades/10.php
@@ -1,4 +1,19 @@
 <?php
 // Atividade 10
 
+$associativo = [
+    [
+        "nome" => "Dionata",
+        "idade" => "89",
+        "cidade" => "Gorpa City",
+        "tchola" => "É raro, mas sempre acontece"
+    ]
+];
+
+foreach ($associativo as $assos) {
+    echo "Nome: " . $assos['nome'] . "\n";
+    echo "Idade: " . $assos['idade'] . "\n";
+    echo "Cidade: " . $assos['cidade'] . "\n";
+    echo "Tchola: " . $assos['tchola'] . "\n";
+}
 ?>
